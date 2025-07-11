3019c228a2a966e1c2da85decddc3a3c5abe5cd3
calegria <calegria@MacBook-Pro-de-calegria.local>
2025-07-11 18:33:25 -0500

    11-07-2025: Login - V1.
    11-07-2025: Login - V1.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * @abstract Componente home.Información general de ZenWk.
+ * @returns
+ */
+const Home = () => {
+  return <div>[Home]</div>;
+};
+
+export default Home;
